1fb3837b8e20e2a6077c4a462e1317585865504c
Cassio Passos <passoscassio29@gmail.com>
2026-09-09 19:22:55 -0300

    feat - fabricacao de picaretas via funcoes estaticas

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,53 @@
+import classes.entidades.PicaretaEntidade;
 import classes.services.PicaretaServico;
 import enums.Materiais;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String> picaretas = new ArrayList<>();
         PicaretaServico picaretaService = new PicaretaServico();
 
-        picaretaService.Fabricar(
+        System.out.println("""
+                  ╔════════════════════════════════════════════════════════════════════╗
+                  ║        VELHO STEVE - FÁBRICA REFATORADA DE PICARETAS MANUAL        ║
+                  ╚════════════════════════════════════════════════════════════════════╝\s
+                """);
+
+        String picareta1 = picaretaService.Fabricar(
                 Materiais.MADEIRA,
                 50,
                 5
                 );
 
-        picaretaService.Fabricar(
+        picaretas.add(picareta1);
+
+        String picareta2 = picaretaService.Fabricar(
                 Materiais.PEDRA,
                 70,
                 8
         );
 
-        picaretaService.Fabricar(
+        picaretas.add(picareta2);
+
+        String picareta3 = picaretaService.Fabricar(
                 Materiais.FERRO,
                 100,
                 12
         );
+
+        picaretas.add(picareta3);
+
+        System.out.println("\nPicaretas no inventario:");
+        for (String picareta : picaretas){
+            System.out.println(" • " + picareta);
+        }
+
+        //minerar
+
+        //reparar
+
+        //estado final
     }
 }
